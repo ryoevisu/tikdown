4794b9bf6863149f905f8f6d3ea0b26adebf5bc1
ryoevisu <ryotakashievisu@gmail.com>
2024-10-11 18:19:22 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,10 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" />
-      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
-        }
+}
